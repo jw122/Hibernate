570d66830d61a2fcc55c7b1e3f3454bbfa204638
Julia Wu <julia_wu@brown.edu>
2014-07-16 12:12:32 -0300

    Create Produto.java

---
+++ Produto.java
@@ -0,0 +1,44 @@
+package br.com.caelum.hibernate;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Produto {
+	
+	private String descricao; 
+	private Long id;
+	private String nome; 
+	private Double preco;
+	
+	@Id @GeneratedValue
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	@Column(name = "descricao", nullable = true, length = 50)
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	public Double getPreco() {
+		return preco;
+	}
+	public void setPreco(Double preco) {
+		this.preco = preco;
+	}
+	 
+}
